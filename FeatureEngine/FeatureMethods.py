945ad4f5152590b7d7ae068442f0d332345c6c4f
Levine <dylan.levine@alereinc.com>
2021-02-02 23:39:55 -0500

    Update feature methods

--- FeatureEngine/FeatureMethods.py
+++ FeatureEngine/FeatureMethods.py
@@ -6,6 +6,12 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+from collections import defaultdict
+from sklearn.metrics.pairwise import euclidean_distances
+from sklearn.preprocessing import StandardScaler
+
 '''
 Two approaches:
 (1) - Classifying transacations as HCP or NOT HCP
@@ -184,6 +190,32 @@ class FeatureEngine(FunctionFeaturizer):
 
 
 
-    
+#Implementation of Principal Feature Analysis to help in identifying key features in an unsupervised learing context
+#https://stats.stackexchange.com/questions/108743/methods-in-r-or-python-to-perform-feature-selection-in-unsupervised-learning?newreg=8eae58509e344faf824deb6cd0b02d0d
+class PFA:
+    def __init__(self, n_features, q=None):
+        self.q = q
+        self.n_features = n_features
 
+    def fit(self, X):
+        if not self.q:
+            self.q = X.shape[1]
+
+        sc = StandardScaler()
+        X = sc.fit_transform(X)
+
+        pca = PCA(n_components=self.q).fit(X)
+        A_q = pca.components_.T
+
+        kmeans = KMeans(n_clusters=self.n_features).fit(A_q)
+        clusters = kmeans.predict(A_q)
+        cluster_centers = kmeans.cluster_centers_
+
+        dists = defaultdict(list)
+        for i, c in enumerate(clusters):
+            dist = euclidean_distances([A_q[i, :]], [cluster_centers[c, :]])[0][0]
+            dists[c].append((i, dist))
+
+        self.indices_ = [sorted(f, key=lambda x: x[1])[0][0] for f in dists.values()]
+        self.features_ = X[:, self.indices_]
 
